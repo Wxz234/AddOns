9a4b8957fac36346c6db4c0aee6aa2cbc291c394
Uzi Wang <429373960@qq.com>
2024-08-20 15:00:43 +0800

    Update WTools.lua
    修复修理bug

--- WTools/WTools.lua
+++ WTools/WTools.lua
@@ -5,6 +5,14 @@ ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, "大脚世界频道")
 local frame = CreateFrame("FRAME");
 frame:RegisterEvent("MERCHANT_SHOW");
 local function eventHandler(self, event, ...)
-	RepairAllItems()
+    if (CanMerchantRepair()) then
+        repairAllCost, _canRepair = GetRepairAllCost();
+        if (_canRepair and repairAllCost > 0) then
+            RepairAllItems();
+            print("修理成功")
+        else
+            print("不需要修理")
+        end
+    end
 end
 frame:SetScript("OnEvent", eventHandler);
